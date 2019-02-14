7913fd675538f699bf44bf81e8e3be8c88388a48
Lily Nguyen <nguyen.lily.p@gmail.com>
2019-02-14 15:04:31 -0800

    more preprocessing stuff

--- adoption-predictor.py
+++ adoption-predictor.py
@@ -6,8 +6,10 @@
 
 import argparse, csv, json, os, pandas, sys
 
-# default file path for training data
+# default file path for training data files
 TRAIN_DATA_PATH = "data/train.csv"
+BREED_LABELS_PATH = "data/breed_labels.csv"
+COLOR_LABELS_PATH = "data/color_labels.csv"
 # default file path for sentiment analysis json files
 SENTIMENT_DATA_PATH = "data/train_sentiment/"
 
@@ -40,13 +42,60 @@ def main():
     for file in os.listdir(SENTIMENT_DATA_PATH):
         with open(SENTIMENT_DATA_PATH + file, "r", encoding = "utf8") as sentiment_file:
             sentiment_json = json.load(sentiment_file)
+            # sentiment will be set as the overall document emotion score * the overall document magnitude score
             sentiment = sentiment_json["documentSentiment"]["magnitude"] * sentiment_json["documentSentiment"]["score"]
             train_data_df.loc[train_data_df["PetID"] == file[:-5], "DescriptionSentiment"] = sentiment
 
     # drop rows that don't have a value for sentiment
     train_data_df = train_data_df[train_data_df.DescriptionSentiment.notnull()]
 
-    train_data_df.to_csv("data/modified_train.csv", index = False)
+    # replace categorical feature values w/ actual text values
+    BREED_DICT = {0: None}
+    COLOR_DICT = {0: None}
+    GENDER_DICT = {1: "Male", 2: "Female", 3: "Mixed"}
+    MATURITYSIZE_DICT = {0: "Not Specified", 1: "Small", 2: "Medium", 3: "Large", 4: "Extra Large"}
+    FURLENGTH_DICT = {0: "Not Specified", 1: "Short", 2: "Medium", 3: "Long"}
+    VACCINATED_DICT = {1: "Yes", 2: "No", 3: "Not Sure"}
+    DEWORMED_DICT = {1: "Yes", 2: "No", 3: "Not Sure"}
+    STERILIZED_DICT ={1: "Yes", 2: "No", 3: "Not Sure"}
+    HEALTH_DICT = {0: "Not Specified", 1: "Healthy", 2: "Minor Injury", 3: "Serious Injury"}
+    with open(BREED_LABELS_PATH, "r", encoding = "utf8") as breeds_file:
+        csv_reader = csv.DictReader(breeds_file)
+        for row in csv_reader:
+            BREED_DICT[int(row["BreedID"])] = row["BreedName"]
+    with open(COLOR_LABELS_PATH, "r", encoding = "utf8") as colors_file:
+        csv_reader = csv.DictReader(colors_file)
+        for row in csv_reader:
+            COLOR_DICT[int(row["ColorID"])] = row["ColorName"]
+
+    train_data_df.Breed1 = train_data_df.Breed1.replace(BREED_DICT)
+    train_data_df.Breed2 = train_data_df.Breed2.replace(BREED_DICT)
+    train_data_df.Color1 = train_data_df.Color1.replace(COLOR_DICT)
+    train_data_df.Color2 = train_data_df.Color2.replace(COLOR_DICT)
+    train_data_df.Color3 = train_data_df.Color3.replace(COLOR_DICT)
+    train_data_df.Gender = train_data_df.Gender.replace(GENDER_DICT)
+    train_data_df.MaturitySize = train_data_df.MaturitySize.replace(MATURITYSIZE_DICT)
+    train_data_df.FurLength = train_data_df.FurLength.replace(FURLENGTH_DICT)
+    train_data_df.Vaccinated = train_data_df.Vaccinated.replace(VACCINATED_DICT)
+    train_data_df.Dewormed = train_data_df.Dewormed.replace(DEWORMED_DICT)
+    train_data_df.Sterilized = train_data_df.Sterilized.replace(STERILIZED_DICT)
+    train_data_df.Health = train_data_df.Health.replace(HEALTH_DICT)
+
+    # combine features that have been split (Breed, Color)
+    train_data_df["Breed"] = train_data_df[["Breed1", "Breed2"]].values.tolist()
+    # ??? How to remove None/nan values
+
+    # do one-hot encoding/dummies for each feature that needs it
+
+    # split data between dogs/cats, and write modified dataframes to new files
+    train_dog_df = train_data_df[train_data_df.Type == 1]
+    train_cat_df = train_data_df[train_data_df.Type == 2]
+    train_dog_df = train_dog_df.drop("Type", axis = 1)
+    train_cat_df = train_cat_df.drop("Type", axis = 1)
+
+    train_dog_df.to_csv("data/train_dog.csv", index = False)
+    train_cat_df.to_csv("data/train_cat.csv", index = False)
+    #train_data_df.to_csv("data/train_modified.csv", index = False)
 
     ### training ###
 
